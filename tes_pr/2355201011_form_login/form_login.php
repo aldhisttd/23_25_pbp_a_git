541c8ad646b0c19643693bb214e9baca752b8079
yana <yanaelsya12@gmail.com>
2025-10-14 09:29:04 +0700

    commit pertama

---
+++ tes_pr/2355201011_form_login/form_login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <h1>username</h1>
+        <input type="text" name="username" id="">
+        <h1>password</h1>
+        <input type="password" name="" id="">
+    </form>
+</body>
+</html>
