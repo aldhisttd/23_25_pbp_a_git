fb1608cb2f3efd4a86ca7109ffbed13dbb9fbf45
Safarrudin Fajri <safarrudinfajri2@gmail.com>
2025-10-14 09:54:57 +0700

    pesan

--- tes_pr/2355201046_form_login/form_login.php
+++ tes_pr/2355201046_form_login/form_login.php
@@ -9,9 +9,9 @@
     <div>
         <form action="proses_login.php" method="POST">
             <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required><br><br>
+            <input type="text" id="username" name="username"><br><br>
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
+            <input type="password" id="password" name="password"><br><br>
             <input type="submit" value="Login">
         </form>
     </div>
