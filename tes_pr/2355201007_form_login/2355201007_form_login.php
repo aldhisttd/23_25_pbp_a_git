7dbd5d96191cefb18c57984133df8ff06d394d7d
triwahyun1 <triwahyuni.insit@gmail.com>
2025-10-14 09:56:36 +0700

    update folder tes_pr

--- tes_pr/2355201007_form_login/2355201007_form_login.php
+++ tes_pr/2355201007_form_login/2355201007_form_login.php
@@ -14,7 +14,6 @@
         <input type="text" id="username" name="username" required><br><br>
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br><br>
-        <input type="submit" value="Login">
         <br>
         <button>Login</button>
     </form>
